2e55929160c8fb19c7e4ca45a828e2c776751aaf
Administrator <Administrator@SKY-20180303004>
2018-11-24 15:23:29 +0800

    现在没有问题

--- src/main/java/com/example/game/controller/helloController.java
+++ src/main/java/com/example/game/controller/helloController.java
@@ -3,22 +3,22 @@ package com.example.game.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.game.domain.User;
 
 @RestController
-@Component
 public class helloController {
-	@Value("${com.neo}")
-	String name;
+
 	@RequestMapping()
 	public String index()
 	{
-		return ""+name;
+		return "asd";
 	}
 
 }
