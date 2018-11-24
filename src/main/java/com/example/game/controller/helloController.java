8e4f319d16b5db2d45b3c21e828da67c1ee440bb
Administrator <Administrator@SKY-20180303004>
2018-11-24 09:19:02 +0800

    实现配置文件的读取

--- src/main/java/com/example/game/controller/helloController.java
+++ src/main/java/com/example/game/controller/helloController.java
@@ -1,15 +1,24 @@
 package com.example.game.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.game.domain.User;
+
 @RestController
+@Component
 public class helloController {
+	@Value("${com.neo}")
+	String name;
 	@RequestMapping()
 	public String index()
 	{
-		
-		return "safsd";
+		return ""+name;
 	}
 
 }
